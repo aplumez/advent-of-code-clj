164add0c22ef06647ad183bcfa8b44fc4dc37907
aplumez <86130733+aplumez@users.noreply.github.com>
2023-12-01 08:57:12 +0100

    2023:01

--- src/advent_of_code_clj/day01.clj
+++ src/advent_of_code_clj/day01.clj
@@ -6,8 +6,36 @@
 (def input (-> (puzzle 2023 1)
                (str/split #"\n")))
 
+(def string->number
+  {"one"   "1"
+   "two"   "2"
+   "three" "3"
+   "four"  "4"
+   "five"  "5"
+   "six"   "6"
+   "seven" "7"
+   "eight" "8"
+   "nine"  "9"})
+
 (def solution1
-  input)
+  (->> input
+       (map #(str/replace % #"[a-z]" ""))
+       (map #(-> (str (first %) (last %))
+                 (parse-long)))
+       (reduce +)))
+
+(defn- compute-digits
+  [string m]
+  (let [re-groups (-> (re-pattern (str "(?=(\\d|" (str/join "|" (keys m)) "))"))
+                      (re-seq string))
+        results (map second re-groups)]
+    (->> results
+         (#(list (first %) (last %)))
+         (map #(or (get m %) %))
+         (apply str)
+         (parse-long))))
 
 (def solution2
-  input)
+  (->> input
+       (map #(compute-digits % string->number))
+       (reduce +)))
