0eb79e0752cbadfa27d00d76a100136c7f1da5ae
aplumez <86130733+aplumez@users.noreply.github.com>
2024-12-01 09:55:59 +0100

    2024:01

--- src/advent_of_code_clj/2024/day01.clj
+++ src/advent_of_code_clj/2024/day01.clj
@@ -1,7 +1,27 @@
 (ns advent-of-code-clj.2024.day01
   (:require
    [advent-of-code-clj.core :refer [puzzle]]
+   [advent-of-code-clj.utils :as u]
    [clojure.string :as string]))
 
-(def input (-> (puzzle 2024 1)
-               (string/split-lines)))
+(def input (->> (puzzle 2024 1)
+                (string/split-lines)
+                (map #(string/split % #"\s+"))
+                (map #(map parse-long %))))
+
+(def solution1 (->> input
+                    (u/transpose)
+                    (map sort)
+                    (u/transpose)
+                    (reduce (fn [acc [a b]]
+                              (+ acc (abs (- a b))))
+                            0)))
+
+(def solution2
+  (let [[left right] (u/transpose input)]
+    (->> left
+         (map (fn [e]
+                (-> (filter #(= e %) right)
+                    (count)
+                    (* e))))
+         (reduce +))))
--- src/advent_of_code_clj/utils.clj
+++ src/advent_of_code_clj/utils.clj
@@ -6,3 +6,6 @@
        (when (. m find)
          (cons {:start (. m start) :end (. m end) :group (. m group)}
                (lazy-seq (step))))))))
+
+(defn transpose [coll]
+  (apply map list coll))
